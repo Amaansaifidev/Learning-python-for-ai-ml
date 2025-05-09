7b1ba9bfc013989a43b58e70c8e61ef4b4164394
amaansaifidev <amaansaifidev@gmail.com>
2025-05-09 20:06:23 +0530

    ternaryoperator-in-python

---
+++ ternaryoperator-in-python/main.py
@@ -0,0 +1,4 @@
+age = int(input("enter your age: "))
+name1 = str(input("enter first name: "))
+name2 = str(input("enter second name: "))
+print(f"your name is {name1}" if age >= 10 else f"your name is {name2}")
