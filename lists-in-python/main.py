92e01317fc030dfcaccd04be8e9dc8fec921bffd
amaansaifidev <amaansaifidev@gmail.com>
2025-05-10 19:21:53 +0530

    revering-in-lists-in-python

--- lists-in-python/main.py
+++ lists-in-python/main.py
@@ -3,6 +3,7 @@ fruits = ["apple", "orange", "pineapple", "banana"]
 #fruits.insert(0, "litchi")
 #fruits.append("litchi")
 fruits.sort()
+fruits.reverse()
 for fruit in fruits:
     print(fruit, end="\n")
 
