05ec7342ea7ce631862b394fe74cafe87e65a83e
amaansaifidev <amaansaifidev@gmail.com>
2025-05-10 19:19:02 +0530

    inserting-in-lists-in-python

--- lists-in-python/main.py
+++ lists-in-python/main.py
@@ -1,5 +1,7 @@
 fruits = ["apple", "orange", "pineapple", "banana"]
 #print(fruits)
-fruits.append("litchi")
+fruits.insert(0, "litchi")
+#fruits.append("litchi")
 for fruit in fruits:
     print(fruit, end="\n")
+
