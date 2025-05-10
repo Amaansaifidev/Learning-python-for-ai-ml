2798294488921797485f8bce97563195ef8905c7
amaansaifidev <amaansaifidev@gmail.com>
2025-05-10 19:19:52 +0530

    sorting-in-lists-in-python

--- lists-in-python/main.py
+++ lists-in-python/main.py
@@ -1,7 +1,8 @@
 fruits = ["apple", "orange", "pineapple", "banana"]
 #print(fruits)
-fruits.insert(0, "litchi")
+#fruits.insert(0, "litchi")
 #fruits.append("litchi")
+fruits.sort()
 for fruit in fruits:
     print(fruit, end="\n")
 
