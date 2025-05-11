cc40af10b850863eb471fd5798151c4a739d720a
amaansaifidev <amaansaifidev@gmail.com>
2025-05-11 20:01:02 +0530

    file-handling-new-improved-in-python

--- filehandling-in-python/main.py
+++ filehandling-in-python/main.py
@@ -1,5 +1,9 @@
+age = "amaan"
+with open("age.txt", "a") as file:
+   file.write(f"{age}\n")
+#with open("age.txt", "r") as file:
+ #   print(file.read())
 #age = "amaan"
-#with open("age.txt", "a") as file:
- #   file.write(f"{age}\n")
-with open("age.txt", "r") as file:
-    print(file.read())
+#with open("age.txt", "x") as file:
+#    file.write(f"{age}\n")
+
